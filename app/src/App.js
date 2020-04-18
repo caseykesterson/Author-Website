57b0d651aef63da9131d251105477b51c0ddbb70
caseykesterson <63170220+caseykesterson@users.noreply.github.com>
2020-04-18 15:00:55 -0400

    Nav bar font, option spacing, and made bar thinner

--- app/src/App.js
+++ app/src/App.js
@@ -36,26 +36,28 @@ export default class App extends React.Component {
 
   render() {
     return <Fragment>
-      <Navbar className = "Navbar" variant = "light" expand="md">
+      <Navbar className = "Navbar" variant = "light" expand="xs">
         <NavbarBrand className="navbar-brand">Casey Kesterson</NavbarBrand>
         <NavbarToggler onClick={this.toggle} />
         <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="navbar-options">
+          <Nav className="navbar">
             <NavItem>
-              <NavLink className="navbar-brand" href="/components/">Books</NavLink>
+              <NavLink className="navbar-options" href="/components/">Books</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className="navbar-brand" href="https://github.com/caseykesterson/Author-Website">Other Published Works</NavLink>
+              <NavLink className="navbar-options" href="https://github.com/caseykesterson/Author-Website">Other Published Works</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className="navbar-brand" href="/components/">About Me</NavLink>
+              <NavLink className="navbar-options" href="/components/">About Me</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className="navbar-brand" href="/components/">Contact Me</NavLink>
+              <NavLink className="navbar-options" href="/components/">Contact Me</NavLink>
             </NavItem>
             </Nav>
         </Collapse>
       </Navbar>
+      <div className="content"
+    />
     </Fragment>
   }
 }
