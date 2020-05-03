0d85aed665cd5bb763848580a7753d42631df691
caseykesterson <63170220+caseykesterson@users.noreply.github.com>
2020-05-03 16:01:30 -0400

    gh-pages prod build

---
+++ app/build/precache-manifest.4c60ddde099655cfdc247a91bba8ac25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2565896eb494ffde4f4c50dcd4498f",
+    "url": "/Author-Website/index.html"
+  },
+  {
+    "revision": "7a0357a2eadbe7bc9134",
+    "url": "/Author-Website/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5cdd4d2cfab22ea98db5",
+    "url": "/Author-Website/static/css/main.7de3351b.chunk.css"
+  },
+  {
+    "revision": "7a0357a2eadbe7bc9134",
+    "url": "/Author-Website/static/js/2.da29231a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Author-Website/static/js/2.da29231a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdd4d2cfab22ea98db5",
+    "url": "/Author-Website/static/js/main.41adcc9d.chunk.js"
+  },
+  {
+    "revision": "7c3827ac3e6ac69b0c5e",
+    "url": "/Author-Website/static/js/runtime-main.21ad9f14.js"
+  },
+  {
+    "revision": "4e7d684fd4477e0bab2813d13ec891c5",
+    "url": "/Author-Website/static/media/headshot.4e7d684f.png"
+  }
+]);
